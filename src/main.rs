55fc7972bfc0e154382a3a8f137af82cd8d8f801
zabl <zzzabl@gmail.com>
2022-02-13 16:43:03 +0300

    use tokio

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::env;
-//use std::io::{BufRead, Write};
-use tokio::net::{TcpStream, TcpListener};
-use tokio::io::{BufWriter, BufReader, WriteHalf, AsyncBufReadExt, AsyncWriteExt};
-
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter, WriteHalf};
+use tokio::net::{TcpListener, TcpStream};
 
 static mut IS_ON: bool = false;
 static POWER: f32 = 123.;
@@ -10,11 +8,13 @@ static POWER: f32 = 123.;
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    let listener = TcpListener::bind(format!("{}:{}", args[1], args[2])).await.unwrap();
+    let listener = TcpListener::bind(format!("{}:{}", args[1], args[2]))
+        .await
+        .unwrap();
     println!("Слушаем: {} {}", args[1], args[2]);
     loop {
         let stream_result = listener.accept().await;
-        if let Ok((stream,addr)) = stream_result {
+        if let Ok((stream, addr)) = stream_result {
             println!("Новое соединение: {}", addr);
             handle_connection(stream).await.unwrap();
         }
@@ -22,16 +22,16 @@ async fn main() {
 }
 
 async fn handle_connection(stream: TcpStream) -> Result<(), String> {
-
     //let mut (read_buf = BufReader::new(stream.into_split());
-    let (mut read, mut write) = tokio::io::split(stream);
+    let (read, write) = tokio::io::split(stream);
 
     let mut write_buf = BufWriter::new(write);
     let mut read_buf = BufReader::new(read);
 
-
     let mut command = String::new();
-    read_buf.read_line(&mut command).await
+    read_buf
+        .read_line(&mut command)
+        .await
         .map_err(|err| err.to_string())?;
     command = command.replace("\n", "");
     println!("command: {}", command);
@@ -46,7 +46,9 @@ async fn handle_connection(stream: TcpStream) -> Result<(), String> {
 async fn do_switch(writer: &mut BufWriter<WriteHalf<TcpStream>>) -> Result<(), String> {
     unsafe {
         IS_ON = !IS_ON;
-        writer.write(IS_ON.to_string().as_bytes()).await
+        writer
+            .write(IS_ON.to_string().as_bytes())
+            .await
             .map_err(|e| e.to_string())?;
         println!("do_switch: return {}", IS_ON);
     };
@@ -57,6 +59,7 @@ async fn get_state(writer: &mut BufWriter<WriteHalf<TcpStream>>) -> Result<(), S
     unsafe {
         let buf = [IS_ON as u8];
         writer.write(&buf).await.map_err(|e| e.to_string())?;
+        writer.flush().await.map_err(|e| e.to_string())?;
         println!("get_state: return {}", IS_ON);
     };
     Ok(())
